15503970f90c6322ae05d41a49fd24c228601109
Qiumix <j487428862@gmail.com>
2026-03-21 21:12:23 +0800

    feat: do some sys_trace

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TASK_MANAGER},
     timer::get_time_us,
 };
 
@@ -40,6 +40,35 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+    // From xv6 lab: syscall
+    // uint64 sys_trace(void) {
+    //   int mask;
+    //   if (argint(0, &mask) < 0)
+    //     return -1;
+    //   myproc()->mask = mask;
+    //   return 0;
+    // }
+    //
+    // pub struct TaskContext {
+    //     /// Ret position after task switching
+    //     ra: usize,
+    //     /// Stack pointer
+    //     sp: usize,
+    //     /// s0-11 register, callee saved
+    //     s: [usize; 12],
+    // }
     trace!("kernel: sys_trace");
-    -1
+    if _trace_request == 0 {
+        unsafe { *(_id as *const u8) as isize }
+    } else if _trace_request == 1 {
+        let data = (_data & 0xFF) as u8;
+        unsafe {
+            *(_id as *mut u8) = data;
+        }
+        0
+    } else if _trace_request == 2 {
+        -1
+    } else {
+        -1
+    }
 }
